1b3a1a36e5da035dbb1042e302d80ac42fc549ef
pownallk <pownallk@gmail.com>
2016-11-28 12:05:31 -0600

    introducing list view an listActivity: added these things to new DailyForecastActivity

---
+++ app/src/main/java/com/karenpownall/android/aca/stormy/ui/DailyForecastActivity.java
@@ -0,0 +1,15 @@
+package com.karenpownall.android.aca.stormy.ui;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+import com.karenpownall.android.aca.stormy.R;
+
+public class DailyForecastActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_forecast);
+    }
+}
