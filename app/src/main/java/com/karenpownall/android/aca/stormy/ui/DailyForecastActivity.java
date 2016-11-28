435b9d898bd211e88730b4b73783f17e63e24247
pownallk <pownallk@gmail.com>
2016-11-28 12:18:20 -0600

    using a default adapter: list item layout and adapter added using android SDK provided components

--- app/src/main/java/com/karenpownall/android/aca/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/karenpownall/android/aca/stormy/ui/DailyForecastActivity.java
@@ -2,6 +2,7 @@ package com.karenpownall.android.aca.stormy.ui;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.karenpownall.android.aca.stormy.R;
 
@@ -12,7 +13,13 @@ public class DailyForecastActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
-        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday",
+                "Thursday", "Friday", "Saturday"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                daysOfTheWeek);
+
+        setListAdapter(adapter);
     }
 
 
